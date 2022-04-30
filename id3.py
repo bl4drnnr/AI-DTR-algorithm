3a34d617e2174edf1802c80223c65c8884755e01
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-30 13:51:38 +0200

    all possible subsets

--- id3.py
+++ id3.py
@@ -23,8 +23,28 @@ CLASSES_INFO_GAIN = informationGain(quantityOfRecordsPerDecisionAttribute)
 
 # Divide input data on subsets by attributes
 ALL_POSSIBLE_SUBSETS = {}
+ALL_POSSIBLE_SUBSETS_COPY = {}
 
 for attr, value in ALL_POSSIBLE_ATTRIBUTES.items():
     if attr != KEY_ATTRIBUTE:
         ALL_POSSIBLE_SUBSETS[attr] = value
 
+for attr, value in ALL_POSSIBLE_SUBSETS.items():
+
+    for record in DATA:
+        for recAttr, recValue in record.items():
+            if attr == recAttr:
+
+                for attrSubset, valueSubset in value.items():
+                    if recValue == valueSubset and ALL_POSSIBLE_SUBSETS_COPY.get(attr) is None:
+                        ALL_POSSIBLE_SUBSETS_COPY[attr] = {}
+                        ALL_POSSIBLE_SUBSETS_COPY[attr][attrSubset] = [record[KEY_ATTRIBUTE]]
+                    elif recValue == valueSubset and ALL_POSSIBLE_SUBSETS_COPY.get(attr) is not None:
+                        if ALL_POSSIBLE_SUBSETS_COPY[attr].get(attrSubset) is None:
+                            ALL_POSSIBLE_SUBSETS_COPY[attr][attrSubset] = {}
+                            ALL_POSSIBLE_SUBSETS_COPY[attr][attrSubset] = [record[KEY_ATTRIBUTE]]
+                        else:
+                            ALL_POSSIBLE_SUBSETS_COPY[attr][attrSubset].append(record[KEY_ATTRIBUTE])
+
+for attr, value in ALL_POSSIBLE_SUBSETS_COPY.items():
+    print(attr, value)
