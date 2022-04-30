4104ab2f3146063ca0c913363180fbf6fa3a142f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-30 10:52:55 +0200

    quantityOfRecordsPerDecisionAttribute

---
+++ common.py
@@ -0,0 +1,12 @@
+
+def informationGain():
+    # gain(X) = info(T) - info_x(T)
+    return
+
+
+def informationEntropy():
+    return
+
+
+def informationEntropyX():
+    return
--- id3.py
+++ id3.py
@@ -1,7 +1,15 @@
-from parser import parseInputData, getAllPossibleAttributes, getKeyAttribute, getDecisionAttributes
+from parser import parseInputData, getAllPossibleAttributes, getKeyAttribute
 
 DATA = parseInputData()
 KEY_ATTRIBUTE = getKeyAttribute()
 ALL_POSSIBLE_ATTRIBUTES = getAllPossibleAttributes()
 INPUT_DATA_LENGTH = len(DATA)
 GENERATED_TREE = []
+
+# Find information entropy for decision attribute
+quantityOfRecordsPerDecisionAttribute = {}
+for record in DATA:
+    if quantityOfRecordsPerDecisionAttribute.get(record[KEY_ATTRIBUTE]) is None:
+        quantityOfRecordsPerDecisionAttribute[record[KEY_ATTRIBUTE]] = 1
+    else:
+        quantityOfRecordsPerDecisionAttribute[record[KEY_ATTRIBUTE]] += 1
