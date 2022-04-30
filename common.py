58558c6e18e78354653269d509fb2a9012d02102
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-30 11:13:08 +0200

    information entropy func

--- common.py
+++ common.py
@@ -1,12 +1,17 @@
+import math
+
 
 def informationGain(X):
     totalSumOfX = 0
 
     if type(X[0]) is int:
         totalSumOfX = sum(X)
+        return informationEntropy(X[0]/totalSumOfX, X[1]/totalSumOfX)
     else:
         for x in X:
             totalSumOfX += x[0]
             totalSumOfX += x[1]
 
-    return
+
+def informationEntropy(x, y):
+    return -1 * x * math.log2(x) - y * math.log2(y)
